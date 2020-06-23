68f7fad33146354a98902e9d2a362144ed873b3a
tarun8718 <tarunkumar8718@gmail.com>
2020-06-23 17:16:11 +0530

    Resolved the redirection issue

--- Adminpage/login/urls.py
+++ Adminpage/login/urls.py
@@ -5,15 +5,13 @@ urlpatterns = [
     path('',views.login,name='login'),
     path('CreateAccount/',views.register,name='register'),
     path('options',views.options),
-    path('upload/',views.upload,name='upload'),
-    path('upload/sendvac',views.sendvac),
+    path('upload',views.upload),
+    path('sendvac',views.sendvac),
     path('viewvac',views.viewvac,name='viewvacancies'),
     path('select',views.select,name='selectapplicants'),
-    path('upload/viewvac',views.viewvac),
-<<<<<<< HEAD
+    #path('upload/viewvac',views.viewvac),
     path('optionsP',views.optionsp),
-=======
-    path('recruitedfaculty/',views.recruitedfaculty,name='recruitedfaculty'),
-    path('upload/recruitedfaculty',views.recruitedfaculty),
->>>>>>> ad45de80bb0e98a64be364d61edaf3ddbf0ddc65
+    path('recruitedfaculty',views.recruitedfaculty,name='recruitedfaculty'),
+    #path('upload/recruitedfaculty',views.recruitedfaculty),
+    
 ]
